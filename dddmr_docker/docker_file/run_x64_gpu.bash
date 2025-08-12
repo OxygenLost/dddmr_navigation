710827a3904609041f8310ccec322f7498b903b9
apola <yan_1whun1@hotmail.com>
2025-08-12 09:43:06 +0400

    unite bash file to gpu dev env

--- dddmr_docker/docker_file/run_x64_gpu.bash
+++ dddmr_docker/docker_file/run_x64_gpu.bash
@@ -2,11 +2,13 @@
 
 xhost +local:docker
 
-
-
 is_cuda=$(docker image ls dddmr | grep 12.6)
 is_l4t_r36=$(docker image ls dddmr | grep l4t_r36)
+
 if [ "$is_cuda" != "" ] ;then
+    echo "Detect image of dddmr:pytorch2.5.1-cuda12.6-cudnn9-tensorrt10.7"
+    echo "Enter ROS_DOMAIN_ID you want for the container."
+    read domain_id
     docker run -it \
         --privileged \
         --network=host \
@@ -15,13 +17,17 @@ if [ "$is_cuda" != "" ] ;then
         --env="NVIDIA_DRIVER_CAPABILITIES=all"\
         --env="DISPLAY" \
         --env="QT_X11_NO_MITSHM=1" \
+        --env="ROS_DOMAIN_ID=${domain_id}" \
         --volume="/tmp:/tmp" \
         --volume="/dev:/dev" \
         --volume="${HOME}/dddmr_bags:/root/dddmr_bags" \
         --volume="${HOME}/dddmr_navigation:/root/dddmr_navigation" \
-        --name="dddmr_humble_dev" \
+        --name="dddmr_humble_cuda_dev" \
         dddmr:pytorch2.5.1-cuda12.6-cudnn9-tensorrt10.7
 elif [ "$is_l4t_r36" != "" ] ;then 
+    echo "Detect image of dddmr:l4t_r36"
+    echo "Enter ROS_DOMAIN_ID you want for the container."
+    read domain_id
     docker run -it \
         --privileged \
         --network=host \
@@ -30,10 +36,11 @@ elif [ "$is_l4t_r36" != "" ] ;then
         --env="QT_X11_NO_MITSHM=1" \
         --env="NVIDIA_VISIBLE_DEVICES=all"\
         --env="NVIDIA_DRIVER_CAPABILITIES=all"\
+        --env="ROS_DOMAIN_ID=${domain_id}" \
         --volume="/dev:/dev" \
         --volume="/tmp:/tmp" \
         --volume="${HOME}/dddmr_bags:/root/dddmr_bags" \
         --volume="${HOME}/dddmr_navigation:/root/dddmr_navigation" \
-        --name="dddmr_humble_dev" \
+        --name="dddmr_humble_l4t_dev" \
         dddmr:l4t_r36
 fi
