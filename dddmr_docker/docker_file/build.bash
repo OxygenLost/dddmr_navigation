e69081bcdad441c742b9f758f9262a9a1357399e
apola <yan_1whun1@hotmail.com>
2025-09-03 11:53:59 +0400

    update build bash

--- dddmr_docker/docker_file/build.bash
+++ dddmr_docker/docker_file/build.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function build_x64(){
+    docker build --network host -t dddmr:x64 -f Dockerfile_x64 . --no-cache
+}
+
 #-----select image
 echo -n "Select image type (x64/l4t/x64_gz): "
 read image_type
@@ -9,12 +13,12 @@ if [[ $image_type == "x64" ]]; then
     read is_cuda
     if [ "$is_cuda" != "${is_cuda#[Yy]}" ] ;then
         echo "----> Creating x64 image with cuda, the x64 image will be created first"
-        docker build --network host -t dddmr:x64 -f Dockerfile_x64 . --no-cache
+        build_x64
         echo "----> Starting second layer with CUDA"
         docker build --network host -t dddmr:pytorch2.5.1-cuda12.6-cudnn9-tensorrt10.7 -f Dockerfile_x64_cuda . --no-cache
     else
         echo "----> Creating x64 image without cuda"
-        docker build --network host -t dddmr:x64 -f Dockerfile_x64 . --no-cache
+        build_x64
     fi
 
 elif [[ $image_type == "l4t" ]]; then
@@ -23,7 +27,7 @@ elif [[ $image_type == "l4t" ]]; then
 
 elif [[ $image_type == "x64_gz" ]]; then
     echo "----> Creating x64 image with cuda, the x64 image will be created first"
-    docker build --network host -t dddmr:x64 -f Dockerfile_x64 . --no-cache
+    build_x64
     echo "----> Starting second layer with gz"
     docker build --network host -t dddmr_gz:x64 -f Dockerfile_x64_gazebo . --no-cache
 
